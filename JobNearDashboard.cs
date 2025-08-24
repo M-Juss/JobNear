76ffc9c84634ec210538181e852e2136b03b7684
M-Juss <jusssayson@gmail.com>
2025-08-24 23:37:29 +0800

    Add project files.

---
+++ JobNearDashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JobNear
+{
+    public partial class JobNearDashboard : Form
+    {
+        public JobNearDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
