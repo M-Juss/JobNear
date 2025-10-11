382d33081649bd3d8cdf96d6e2a06ad1cb51af46
M-Juss <jusssayson@gmail.com>
2025-10-11 23:22:05 +0800

    usenotif model

--- Models/UserNotificationModel.cs
+++ Models/UserNotificationModel.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace JobNear.Models
 {
-    internal class UserNotificationModel
+    public class UserNotificationModel
     {
+        [BsonId] 
+        [BsonRepresentation(BsonType.ObjectId)]
+        string Id { get; set; }
+        string Key { get; set; }
+        string HeaderMessage { get; set; }
+        string Remarks { get; set; }
+        string Type { get; set; }
+        DateTime Date{ get; set; }
     }
 }
