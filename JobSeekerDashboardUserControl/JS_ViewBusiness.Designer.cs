e52fcab32de05f4c5ef7d0711eebf444f98c7221
M-Juss <jusssayson@gmail.com>
2025-11-11 20:28:15 +0800

    add view for busines in JP pov

---
+++ JobSeekerDashboardUserControl/JS_ViewBusiness.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace JobNear.JobSeekerDashboardUserControl
+{
+    partial class JS_ViewBusiness
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.business_panel = new System.Windows.Forms.Panel();
+            this.edit_button = new System.Windows.Forms.Button();
+            this.description_label = new System.Windows.Forms.Label();
+            this.address_label = new System.Windows.Forms.Label();
+            this.name_label = new System.Windows.Forms.Label();
+            this.company_logo_picturebox = new System.Windows.Forms.PictureBox();
+            this.joblist_flowlayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.business_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.company_logo_picturebox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.joblist_flowlayout);
+            this.panel1.Controls.Add(this.business_panel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1375, 868);
+            this.panel1.TabIndex = 0;
+            // 
+            // business_panel
+            // 
+            this.business_panel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.business_panel.Controls.Add(this.edit_button);
+            this.business_panel.Controls.Add(this.description_label);
+            this.business_panel.Controls.Add(this.address_label);
+            this.business_panel.Controls.Add(this.name_label);
+            this.business_panel.Controls.Add(this.company_logo_picturebox);
+            this.business_panel.Location = new System.Drawing.Point(161, 54);
+            this.business_panel.Margin = new System.Windows.Forms.Padding(4);
+            this.business_panel.Name = "business_panel";
+            this.business_panel.Size = new System.Drawing.Size(1067, 274);
+            this.business_panel.TabIndex = 1;
+            // 
+            // edit_button
+            // 
+            this.edit_button.Font = new System.Drawing.Font("Poppins SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edit_button.ForeColor = System.Drawing.Color.White;
+            this.edit_button.Location = new System.Drawing.Point(948, 16);
+            this.edit_button.Margin = new System.Windows.Forms.Padding(4);
+            this.edit_button.Name = "edit_button";
+            this.edit_button.Size = new System.Drawing.Size(100, 34);
+            this.edit_button.TabIndex = 6;
+            this.edit_button.Text = "Edit";
+            this.edit_button.UseVisualStyleBackColor = true;
+            // 
+            // description_label
+            // 
+            this.description_label.AutoEllipsis = true;
+            this.description_label.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description_label.Location = new System.Drawing.Point(271, 90);
+            this.description_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.description_label.Name = "description_label";
+            this.description_label.Size = new System.Drawing.Size(667, 100);
+            this.description_label.TabIndex = 5;
+            // 
+            // address_label
+            // 
+            this.address_label.AutoEllipsis = true;
+            this.address_label.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address_label.ForeColor = System.Drawing.Color.Gray;
+            this.address_label.Location = new System.Drawing.Point(271, 203);
+            this.address_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.address_label.Name = "address_label";
+            this.address_label.Size = new System.Drawing.Size(667, 27);
+            this.address_label.TabIndex = 4;
+            // 
+            // name_label
+            // 
+            this.name_label.AllowDrop = true;
+            this.name_label.AutoEllipsis = true;
+            this.name_label.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_label.Location = new System.Drawing.Point(268, 41);
+            this.name_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(669, 44);
+            this.name_label.TabIndex = 3;
+            // 
+            // company_logo_picturebox
+            // 
+            this.company_logo_picturebox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.company_logo_picturebox.Location = new System.Drawing.Point(45, 46);
+            this.company_logo_picturebox.Margin = new System.Windows.Forms.Padding(4);
+            this.company_logo_picturebox.Name = "company_logo_picturebox";
+            this.company_logo_picturebox.Size = new System.Drawing.Size(199, 184);
+            this.company_logo_picturebox.TabIndex = 0;
+            this.company_logo_picturebox.TabStop = false;
+            // 
+            // joblist_flowlayout
+            // 
+            this.joblist_flowlayout.Location = new System.Drawing.Point(161, 410);
+            this.joblist_flowlayout.Margin = new System.Windows.Forms.Padding(4);
+            this.joblist_flowlayout.Name = "joblist_flowlayout";
+            this.joblist_flowlayout.Size = new System.Drawing.Size(1067, 399);
+            this.joblist_flowlayout.TabIndex = 3;
+            // 
+            // JS_ViewBusiness
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "JS_ViewBusiness";
+            this.Size = new System.Drawing.Size(1375, 868);
+            this.panel1.ResumeLayout(false);
+            this.business_panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.company_logo_picturebox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel business_panel;
+        private System.Windows.Forms.Button edit_button;
+        private System.Windows.Forms.Label description_label;
+        private System.Windows.Forms.Label address_label;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.PictureBox company_logo_picturebox;
+        private System.Windows.Forms.FlowLayoutPanel joblist_flowlayout;
+    }
+}
