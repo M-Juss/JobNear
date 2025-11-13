2395a0d74fc50960f2ab0cc2065048289c73045d
M-Juss <jusssayson@gmail.com>
2025-11-13 22:37:35 +0800

    display job details in when joblist click

--- JobSeekerDashboardUserControl/JS_Dashboard.Designer.cs
+++ JobSeekerDashboardUserControl/JS_Dashboard.Designer.cs
@@ -77,7 +77,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.ForeColor = System.Drawing.Color.DimGray;
+            this.label10.ForeColor = System.Drawing.Color.Black;
             this.label10.Location = new System.Drawing.Point(8, 550);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(703, 34);
@@ -89,7 +89,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.DimGray;
+            this.label9.ForeColor = System.Drawing.Color.Black;
             this.label9.Location = new System.Drawing.Point(48, 466);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(651, 28);
@@ -100,7 +100,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.DimGray;
+            this.label8.ForeColor = System.Drawing.Color.Black;
             this.label8.Location = new System.Drawing.Point(48, 505);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(468, 28);
@@ -111,7 +111,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.DimGray;
+            this.label7.ForeColor = System.Drawing.Color.Black;
             this.label7.Location = new System.Drawing.Point(8, 424);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(490, 34);
@@ -122,7 +122,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.DimGray;
+            this.label6.ForeColor = System.Drawing.Color.Black;
             this.label6.Location = new System.Drawing.Point(48, 379);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(371, 28);
@@ -133,7 +133,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.ForeColor = System.Drawing.Color.Black;
             this.label5.Location = new System.Drawing.Point(48, 346);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(650, 28);
@@ -144,7 +144,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.DimGray;
+            this.label4.ForeColor = System.Drawing.Color.Black;
             this.label4.Location = new System.Drawing.Point(10, 311);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(260, 34);
@@ -155,7 +155,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Poppins", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(9, 267);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(348, 42);
@@ -179,7 +179,7 @@
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Poppins", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.ForeColor = System.Drawing.Color.DimGray;
-            this.label14.Location = new System.Drawing.Point(72, 51);
+            this.label14.Location = new System.Drawing.Point(161, 49);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(146, 84);
             this.label14.TabIndex = 6;
@@ -190,7 +190,7 @@
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.Color.DimGray;
-            this.label13.Location = new System.Drawing.Point(80, 10);
+            this.label13.Location = new System.Drawing.Point(3, 0);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(226, 37);
             this.label13.TabIndex = 5;
@@ -212,7 +212,7 @@
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Poppins", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.Color.DimGray;
-            this.label12.Location = new System.Drawing.Point(76, 51);
+            this.label12.Location = new System.Drawing.Point(168, 37);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(146, 84);
             this.label12.TabIndex = 5;
@@ -224,7 +224,7 @@
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.DimGray;
-            this.label11.Location = new System.Drawing.Point(83, 12);
+            this.label11.Location = new System.Drawing.Point(3, 0);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(235, 37);
             this.label11.TabIndex = 4;
--- JobSeekerDashboardUserControl/JS_ViewJobList.cs
+++ JobSeekerDashboardUserControl/JS_ViewJobList.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using JobNear.Controllers;
+using JobNear.Models;
+using JobNear.Services; 
+using JobNear.Styles;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using JobNear.Styles;
 
 namespace JobNear.JobSeekerDashboardUserControl
 {
@@ -18,9 +23,11 @@ namespace JobNear.JobSeekerDashboardUserControl
             InitializeComponent();
             PanelStyles.RoundedPanel(business_panel, 20, Color.White);
             PanelStyles.RoundedPanel(job_panel, 20, Color.White);
+            LoadJobDetails(id);
+
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void panel1_Paint(object sender, PaintEventArgs e)  
         {
 
         }
@@ -28,6 +35,32 @@ namespace JobNear.JobSeekerDashboardUserControl
         private void label4_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public async void LoadJobDetails(string jobId)
+        {
+            var job = await MongoDbServices.JobPosterJobPosting
+                .Find(x => x.Id == jobId)
+                .FirstOrDefaultAsync();
+
+            if (job != null) { 
+                jobtitle_label.Text = job.JobPosition;
+                jobinfo_label.Text = $"{job.JobWorkModel} | {job.JobEmploymentType}" ;
+                about_label.Text = job.JobAbout;
+                responsibilities_label.Text = job.JobResponsibilities;
+                preffered_label.Text = job.JobPreferredQualification;
+                minimum_label.Text = job.JobMinimumQualification;
+                status_label.Text = job.JobStatus;
+
+                if (job.JobHourlyRate == 0) {
+                    rate_label.Text = $"Monthly Salary: ₱{job.JobMonthlyRate}";
+                } else rate_label.Text = $"Hourly Rate: ₱{job.JobHourlyRate}";
+
+                UserController.SetJobPostStatus(status_label, job.JobStatus);
+
+                
+
+            }
         }
     }
 }
