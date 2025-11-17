cf86b1e35ca3e13cae373a521a3b53a5c622d0e0
M-Juss <jusssayson@gmail.com>
2025-11-17 15:19:24 +0800

    fix Admin Account model

--- Models/AdminAccountModel.cs
+++ Models/AdminAccountModel.cs
@@ -12,8 +12,11 @@ namespace JobNear.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public string Email { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
+        public string Fullame { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
 
     }
 }
