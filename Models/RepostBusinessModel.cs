d05159814614c1066c29d99f269e2b7cc0558281
M-Juss <jusssayson@gmail.com>
2025-11-19 12:58:17 +0800

    Add report business model

--- Models/RepostBusinessModel.cs
+++ Models/RepostBusinessModel.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace JobNear.Models
 {
     public class RepostBusinessModel
     {
-
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Complainant { get; set; }
+        public string Complainee { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public List<SupportingDocument> SupportingDocuments { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
     }
 }
