a1183bec6faaa8c1c7f5eebd658db0481625c213
M-Juss <jusssayson@gmail.com>
2025-12-02 23:02:16 +0800

    fix some ui

--- Forms/JobPosterDashboardForm.Designer.cs
+++ Forms/JobPosterDashboardForm.Designer.cs
@@ -50,8 +50,9 @@
             this.JP_sidebar_panel.Controls.Add(this.jobnear_button);
             this.JP_sidebar_panel.Dock = System.Windows.Forms.DockStyle.Left;
             this.JP_sidebar_panel.Location = new System.Drawing.Point(0, 0);
+            this.JP_sidebar_panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.JP_sidebar_panel.Name = "JP_sidebar_panel";
-            this.JP_sidebar_panel.Size = new System.Drawing.Size(200, 705);
+            this.JP_sidebar_panel.Size = new System.Drawing.Size(267, 868);
             this.JP_sidebar_panel.TabIndex = 0;
             // 
             // notification_button
@@ -59,11 +60,13 @@
             this.notification_button.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.notification_button.Dock = System.Windows.Forms.DockStyle.Top;
             this.notification_button.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.notification_button.Location = new System.Drawing.Point(0, 196);
+            this.notification_button.Location = new System.Drawing.Point(0, 240);
+            this.notification_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.notification_button.Name = "notification_button";
-            this.notification_button.Size = new System.Drawing.Size(200, 49);
+            this.notification_button.Size = new System.Drawing.Size(267, 60);
             this.notification_button.TabIndex = 6;
-            this.notification_button.Text = "🔔   Notifications";
+            this.notification_button.Text = "   🔔   Notifications";
+            this.notification_button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.notification_button.UseVisualStyleBackColor = false;
             this.notification_button.Click += new System.EventHandler(this.notification_button_Click);
             // 
@@ -73,9 +76,10 @@
             this.log_out_button.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.log_out_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.log_out_button.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.log_out_button.Location = new System.Drawing.Point(0, 665);
+            this.log_out_button.Location = new System.Drawing.Point(0, 819);
+            this.log_out_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.log_out_button.Name = "log_out_button";
-            this.log_out_button.Size = new System.Drawing.Size(200, 40);
+            this.log_out_button.Size = new System.Drawing.Size(267, 49);
             this.log_out_button.TabIndex = 5;
             this.log_out_button.Text = "↩️ Logout";
             this.log_out_button.UseVisualStyleBackColor = false;
@@ -86,11 +90,13 @@
             this.view_business_button.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.view_business_button.Dock = System.Windows.Forms.DockStyle.Top;
             this.view_business_button.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.view_business_button.Location = new System.Drawing.Point(0, 147);
+            this.view_business_button.Location = new System.Drawing.Point(0, 180);
+            this.view_business_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.view_business_button.Name = "view_business_button";
-            this.view_business_button.Size = new System.Drawing.Size(200, 49);
+            this.view_business_button.Size = new System.Drawing.Size(267, 60);
             this.view_business_button.TabIndex = 4;
-            this.view_business_button.Text = "🗂️   My Businesses";
+            this.view_business_button.Text = "     🗂️   My Businesses";
+            this.view_business_button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.view_business_button.UseCompatibleTextRendering = true;
             this.view_business_button.UseVisualStyleBackColor = false;
             this.view_business_button.Click += new System.EventHandler(this.view_business_button_Click);
@@ -100,11 +106,13 @@
             this.business_button.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.business_button.Dock = System.Windows.Forms.DockStyle.Top;
             this.business_button.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.business_button.Location = new System.Drawing.Point(0, 98);
+            this.business_button.Location = new System.Drawing.Point(0, 120);
+            this.business_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.business_button.Name = "business_button";
-            this.business_button.Size = new System.Drawing.Size(200, 49);
+            this.business_button.Size = new System.Drawing.Size(267, 60);
             this.business_button.TabIndex = 2;
-            this.business_button.Text = "🏢   Register Business";
+            this.business_button.Text = "   🏢   Register Business";
+            this.business_button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.business_button.UseVisualStyleBackColor = false;
             this.business_button.Click += new System.EventHandler(this.business_button_Click);
             // 
@@ -113,11 +121,13 @@
             this.dashboard_button.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.dashboard_button.Dock = System.Windows.Forms.DockStyle.Top;
             this.dashboard_button.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dashboard_button.Location = new System.Drawing.Point(0, 49);
+            this.dashboard_button.Location = new System.Drawing.Point(0, 60);
+            this.dashboard_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dashboard_button.Name = "dashboard_button";
-            this.dashboard_button.Size = new System.Drawing.Size(200, 49);
+            this.dashboard_button.Size = new System.Drawing.Size(267, 60);
             this.dashboard_button.TabIndex = 1;
-            this.dashboard_button.Text = "📊 Dashboard";
+            this.dashboard_button.Text = "   📊 Dashboard";
+            this.dashboard_button.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.dashboard_button.UseVisualStyleBackColor = false;
             this.dashboard_button.Click += new System.EventHandler(this.dashboard_button_Click);
             // 
@@ -126,26 +136,29 @@
             this.jobnear_button.Dock = System.Windows.Forms.DockStyle.Top;
             this.jobnear_button.Image = global::JobNear.Properties.Resources.JobNearDashboardLogo;
             this.jobnear_button.Location = new System.Drawing.Point(0, 0);
+            this.jobnear_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.jobnear_button.Name = "jobnear_button";
-            this.jobnear_button.Size = new System.Drawing.Size(200, 49);
+            this.jobnear_button.Size = new System.Drawing.Size(267, 60);
             this.jobnear_button.TabIndex = 0;
             this.jobnear_button.UseVisualStyleBackColor = true;
             // 
             // sidebar_panel
             // 
             this.sidebar_panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.sidebar_panel.Location = new System.Drawing.Point(200, 0);
+            this.sidebar_panel.Location = new System.Drawing.Point(267, 0);
+            this.sidebar_panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sidebar_panel.Name = "sidebar_panel";
-            this.sidebar_panel.Size = new System.Drawing.Size(1031, 705);
+            this.sidebar_panel.Size = new System.Drawing.Size(1374, 868);
             this.sidebar_panel.TabIndex = 1;
             // 
             // JobPosterDashboardForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1231, 705);
+            this.ClientSize = new System.Drawing.Size(1641, 868);
             this.Controls.Add(this.sidebar_panel);
             this.Controls.Add(this.JP_sidebar_panel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "JobPosterDashboardForm";
             this.Text = "Job Poster";
             this.JP_sidebar_panel.ResumeLayout(false);
--- JobPosterDashboardUserControl/JP_BusinessDetails.Designer.cs
+++ JobPosterDashboardUserControl/JP_BusinessDetails.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.business_panel = new System.Windows.Forms.Panel();
-            this.edit_button = new System.Windows.Forms.Button();
+            this.footer_label = new System.Windows.Forms.Label();
             this.description_label = new System.Windows.Forms.Label();
-            this.address_label = new System.Windows.Forms.Label();
             this.name_label = new System.Windows.Forms.Label();
+            this.edit_button = new System.Windows.Forms.Button();
             this.company_logo_picturebox = new System.Windows.Forms.PictureBox();
             this.post_job_button = new System.Windows.Forms.Button();
             this.joblist_flowlayout = new System.Windows.Forms.FlowLayoutPanel();
@@ -47,65 +47,68 @@
             // business_panel
             // 
             this.business_panel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.business_panel.Controls.Add(this.edit_button);
+            this.business_panel.Controls.Add(this.footer_label);
             this.business_panel.Controls.Add(this.description_label);
-            this.business_panel.Controls.Add(this.address_label);
             this.business_panel.Controls.Add(this.name_label);
+            this.business_panel.Controls.Add(this.edit_button);
             this.business_panel.Controls.Add(this.company_logo_picturebox);
-            this.business_panel.Location = new System.Drawing.Point(22, 79);
+            this.business_panel.Location = new System.Drawing.Point(29, 97);
+            this.business_panel.Margin = new System.Windows.Forms.Padding(4);
             this.business_panel.Name = "business_panel";
-            this.business_panel.Size = new System.Drawing.Size(984, 223);
+            this.business_panel.Size = new System.Drawing.Size(1312, 274);
             this.business_panel.TabIndex = 0;
             // 
-            // edit_button
+            // footer_label
             // 
-            this.edit_button.Font = new System.Drawing.Font("Poppins SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.edit_button.ForeColor = System.Drawing.Color.White;
-            this.edit_button.Location = new System.Drawing.Point(892, 20);
-            this.edit_button.Name = "edit_button";
-            this.edit_button.Size = new System.Drawing.Size(75, 30);
-            this.edit_button.TabIndex = 6;
-            this.edit_button.Text = "Edit";
-            this.edit_button.UseVisualStyleBackColor = true;
+            this.footer_label.AutoEllipsis = true;
+            this.footer_label.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.footer_label.ForeColor = System.Drawing.Color.Gray;
+            this.footer_label.Location = new System.Drawing.Point(313, 223);
+            this.footer_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.footer_label.Name = "footer_label";
+            this.footer_label.Size = new System.Drawing.Size(864, 27);
+            this.footer_label.TabIndex = 10;
             // 
             // description_label
             // 
             this.description_label.AutoEllipsis = true;
-            this.description_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.description_label.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.description_label.Location = new System.Drawing.Point(242, 72);
+            this.description_label.Font = new System.Drawing.Font("Poppins", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.description_label.Location = new System.Drawing.Point(313, 67);
+            this.description_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.description_label.Name = "description_label";
-            this.description_label.Size = new System.Drawing.Size(628, 91);
-            this.description_label.TabIndex = 5;
-            // 
-            // address_label
-            // 
-            this.address_label.AutoEllipsis = true;
-            this.address_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.address_label.Font = new System.Drawing.Font("Poppins", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.address_label.ForeColor = System.Drawing.Color.Gray;
-            this.address_label.Location = new System.Drawing.Point(244, 175);
-            this.address_label.Name = "address_label";
-            this.address_label.Size = new System.Drawing.Size(625, 30);
-            this.address_label.TabIndex = 4;
+            this.description_label.Size = new System.Drawing.Size(864, 166);
+            this.description_label.TabIndex = 11;
             // 
             // name_label
             // 
             this.name_label.AllowDrop = true;
             this.name_label.AutoEllipsis = true;
-            this.name_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.name_label.Font = new System.Drawing.Font("Poppins SemiBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.name_label.Location = new System.Drawing.Point(242, 20);
+            this.name_label.Font = new System.Drawing.Font("Poppins", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name_label.Location = new System.Drawing.Point(307, 23);
+            this.name_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.name_label.Name = "name_label";
-            this.name_label.Size = new System.Drawing.Size(628, 36);
-            this.name_label.TabIndex = 3;
+            this.name_label.Size = new System.Drawing.Size(869, 44);
+            this.name_label.TabIndex = 9;
+            // 
+            // edit_button
+            // 
+            this.edit_button.Font = new System.Drawing.Font("Poppins SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edit_button.ForeColor = System.Drawing.Color.White;
+            this.edit_button.Location = new System.Drawing.Point(1189, 25);
+            this.edit_button.Margin = new System.Windows.Forms.Padding(4);
+            this.edit_button.Name = "edit_button";
+            this.edit_button.Size = new System.Drawing.Size(100, 37);
+            this.edit_button.TabIndex = 6;
+            this.edit_button.Text = "Edit";
+            this.edit_button.UseVisualStyleBackColor = true;
             // 
             // company_logo_picturebox
             // 
             this.company_logo_picturebox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.company_logo_picturebox.Location = new System.Drawing.Point(24, 20);
+            this.company_logo_picturebox.Location = new System.Drawing.Point(32, 25);
+            this.company_logo_picturebox.Margin = new System.Windows.Forms.Padding(4);
             this.company_logo_picturebox.Name = "company_logo_picturebox";
-            this.company_logo_picturebox.Size = new System.Drawing.Size(196, 185);
+            this.company_logo_picturebox.Size = new System.Drawing.Size(261, 227);
             this.company_logo_picturebox.TabIndex = 0;
             this.company_logo_picturebox.TabStop = false;
             // 
@@ -116,18 +119,20 @@
             this.post_job_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.post_job_button.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.post_job_button.ForeColor = System.Drawing.Color.White;
-            this.post_job_button.Location = new System.Drawing.Point(22, 324);
+            this.post_job_button.Location = new System.Drawing.Point(29, 399);
+            this.post_job_button.Margin = new System.Windows.Forms.Padding(4);
             this.post_job_button.Name = "post_job_button";
-            this.post_job_button.Size = new System.Drawing.Size(128, 35);
+            this.post_job_button.Size = new System.Drawing.Size(171, 43);
             this.post_job_button.TabIndex = 1;
             this.post_job_button.Text = "Post a Job";
             this.post_job_button.UseVisualStyleBackColor = false;
             // 
             // joblist_flowlayout
             // 
-            this.joblist_flowlayout.Location = new System.Drawing.Point(22, 366);
+            this.joblist_flowlayout.Location = new System.Drawing.Point(29, 450);
+            this.joblist_flowlayout.Margin = new System.Windows.Forms.Padding(4);
             this.joblist_flowlayout.Name = "joblist_flowlayout";
-            this.joblist_flowlayout.Size = new System.Drawing.Size(982, 314);
+            this.joblist_flowlayout.Size = new System.Drawing.Size(1309, 386);
             this.joblist_flowlayout.TabIndex = 2;
             // 
             // sidebar_panel
@@ -140,17 +145,19 @@
             this.sidebar_panel.Controls.Add(this.status_lbl);
             this.sidebar_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.sidebar_panel.Location = new System.Drawing.Point(0, 0);
+            this.sidebar_panel.Margin = new System.Windows.Forms.Padding(4);
             this.sidebar_panel.Name = "sidebar_panel";
-            this.sidebar_panel.Size = new System.Drawing.Size(1031, 705);
+            this.sidebar_panel.Size = new System.Drawing.Size(1375, 868);
             this.sidebar_panel.TabIndex = 4;
             // 
             // prev_lbl
             // 
             this.prev_lbl.AutoSize = true;
             this.prev_lbl.Font = new System.Drawing.Font("Poppins SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.prev_lbl.Location = new System.Drawing.Point(17, 13);
+            this.prev_lbl.Location = new System.Drawing.Point(23, 16);
+            this.prev_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.prev_lbl.Name = "prev_lbl";
-            this.prev_lbl.Size = new System.Drawing.Size(102, 28);
+            this.prev_lbl.Size = new System.Drawing.Size(125, 36);
             this.prev_lbl.TabIndex = 20;
             this.prev_lbl.Text = "◀️ Previous";
             this.prev_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -159,21 +166,23 @@
             // status_lbl
             // 
             this.status_lbl.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.status_lbl.Location = new System.Drawing.Point(22, 49);
+            this.status_lbl.Location = new System.Drawing.Point(29, 60);
+            this.status_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.status_lbl.Name = "status_lbl";
-            this.status_lbl.Size = new System.Drawing.Size(984, 37);
+            this.status_lbl.Size = new System.Drawing.Size(1312, 46);
             this.status_lbl.TabIndex = 5;
             this.status_lbl.Text = "Status";
             this.status_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // JP_BusinessDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.Controls.Add(this.sidebar_panel);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "JP_BusinessDetails";
-            this.Size = new System.Drawing.Size(1031, 705);
+            this.Size = new System.Drawing.Size(1375, 868);
             this.business_panel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.company_logo_picturebox)).EndInit();
             this.sidebar_panel.ResumeLayout(false);
@@ -187,13 +196,13 @@
         private System.Windows.Forms.Panel business_panel;
         private System.Windows.Forms.PictureBox company_logo_picturebox;
         private System.Windows.Forms.Button edit_button;
-        private System.Windows.Forms.Label description_label;
-        private System.Windows.Forms.Label address_label;
-        private System.Windows.Forms.Label name_label;
         private System.Windows.Forms.Button post_job_button;
         private System.Windows.Forms.FlowLayoutPanel joblist_flowlayout;
         private System.Windows.Forms.Panel sidebar_panel;
         private System.Windows.Forms.Label status_lbl;
         private System.Windows.Forms.Label prev_lbl;
+        private System.Windows.Forms.Label footer_label;
+        private System.Windows.Forms.Label description_label;
+        private System.Windows.Forms.Label name_label;
     }
 }
